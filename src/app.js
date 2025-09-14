91054d8a1040751947f5bcc74a9f1801a60deac1
Irfan140 <irfanmehmud140@gmail.com>
2025-09-14 15:22:35 +0530

    Add authentication feature with signup and signin endpoints
    - Created auth controller and routes for user signup and signin
    - Implemented user creation with password hashing and JWT token generation
    - Added validation for signup and signin requests using Zod
    - Introduced cookie management for JWT tokens
    - Updated environment configuration and package dependencies

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
+import { timestamp } from 'drizzle-orm/gel-core';
 
 const app = express();
 
@@ -23,5 +25,19 @@ app.get('/', (req, res) => {
   logger.info('Hello from / route');
   res.status(200).send('Hello from Api...');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'API is running !' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
