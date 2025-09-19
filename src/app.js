ccde0b9eac2851f807d6b5335c28bb568f0cb4a9
Irfan140 <irfanmehmud140@gmail.com>
2025-09-19 14:44:54 +0530

    Implement testing using Jest

--- src/app.js
+++ src/app.js
@@ -44,4 +44,9 @@ app.get('/api', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 
+// For any other routes that is not there
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
 export default app;
